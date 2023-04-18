c61b99d35ffb3e94f965113684d5420679e19859
buentellopc <buentellopc@gmail.com>
2023-04-17 21:01:29 -0600

    Journaler API: first commit

---
+++ src/test/kotlin/com/jouernaler/journalerapi/JournalerApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jouernaler.journalerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JournalerApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
